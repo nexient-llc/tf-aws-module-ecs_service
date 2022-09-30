3abcaafa2e43dadd4c897aa1873f08deb5faab1e
Cameron Wright <cwright@nexient.com>
2022-09-30 08:52:04 -0700

    outputs align with arn

--- outputs.tf
+++ outputs.tf
@@ -29,45 +29,10 @@ output "name" {
 
 output "alb_name" {
   description = "The ARN suffix of the ALB"
-  value       = module.alb.alb_name
+  value       = module.alb.load_balancer_name
 }
 
 output "alb_arn" {
   description = "The ARN of the ALB"
-  value       = module.alb.alb_arn
-}
-
-output "alb_arn_suffix" {
-  description = "The ARN suffix of the ALB"
-  value       = module.alb.alb_arn_suffix
-}
-
-output "alb_dns_name" {
-  description = "DNS name of ALB"
-  value       = module.alb.alb_dns_name
-}
-
-output "alb_zone_id" {
-  description = "The ID of the zone which ALB is provisioned"
-  value       = module.alb.alb_zone_id
-}
-
-output "security_group_id" {
-  description = "The security group ID of the ALB"
-  value       = module.alb.security_group_id
-}
-
-output "default_target_group_arn" {
-  description = "The default target group ARN"
-  value       = module.alb.default_target_group_arn
-}
-
-output "http_listener_arn" {
-  description = "The ARN of the HTTP listener"
-  value       = module.alb.http_listener_arn
-}
-
-output "listener_arns" {
-  description = "A list of all the listener ARNs"
-  value       = module.alb.listener_arns
+  value       = module.alb.load_balancer_type
 }
